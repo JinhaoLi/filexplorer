6555a4d5857f4ac7cf5573296a84e3eaff7f92a4
JIL <q1521983309@63.com>
2020-04-10 21:27:37 +0800

    点击fragment中的recycleView项目测试

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-安卓的文件管理器
+
+********文件管理器******** 
+====
+
+|![ico](app/src/main/ic_launcher-web.png)|
+|:---------:|
+|文件管理器|
+
+
+
+
+##Preview
+|![Preview](./preview/1.png)|![Preview](./preview/1.png)|![Preview](./preview/1.png)|
+|:---------:|:---------:|:---------:|
+
+
+##Download
+|来源|说明|
+|:---:|:---:|
+|[coolapk](https://developer.coolapk.com/)|**酷安**网下载|
+|[github](https://github.com/JinhaoLi)|**github**下载|
+
+##使用
+```@Override
+protected void onDestroy() {
+    EventBus.getDefault().unregister(this);
+    super.onDestroy();
+}```
--- app/src/androidTest/java/com/jil/filexplorer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jil/filexplorer/ExampleInstrumentedTest.java
@@ -1,23 +1,31 @@
 package com.jil.filexplorer;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-
 import com.jil.filexplorer.activity.MainActivity;
+import com.jil.filexplorer.bean.Item;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.app.PendingIntent.getActivity;
-import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -26,6 +34,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
     @Test
     public void useAppContext() {
          //Context of the app under test.
@@ -34,11 +46,56 @@ public class ExampleInstrumentedTest {
         Intent i =new Intent(appContext, MainActivity.class);
         getActivity(appContext,0,i,0);
 
-        onView(withId(R.id.imageButton3)).perform(click());
+        //onView(withId(R.id.imageButton3)).perform(click());
+        Espresso.onView(withId(R.id.editText)).perform(click());
+
         try {
             Thread.sleep( 5000 );
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void viewClick(){
+        Matcher<View> matcher = ViewMatchers.withText("Backup");
+
+        ViewInteraction vi =Espresso.onView(matcher);
+
+        vi.perform(ViewActions.longClick());
+
+        try {
+            Thread.sleep( 5000 );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void textReplace(){
+        Matcher<View> matcher = ViewMatchers.withId(R.id.editText);
+
+        ViewInteraction vi =Espresso.onView(matcher);
+
+        vi.perform(ViewActions.replaceText("hello,world"));
+
+    }
+
+    @Test
+    public void testList() throws InterruptedException {
+        //instanceOf(String.class);
+        Thread.sleep( 1000 );
+        Matcher<View> matcher = ViewMatchers.withText("Backup");
+
+        ViewInteraction vi =Espresso.onView(matcher);
+
+        vi.perform(ViewActions.longClick());
+        Thread.sleep( 1000 );
+        Espresso.onData(Matchers.anything())
+                .inAdapterView(ViewMatchers.hasChildCount(7))
+                .atPosition(6)
+                .perform(ViewActions.click());
+
+        Thread.sleep( 5000 );
     }
 }
